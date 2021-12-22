b51a7ebf1b00e60c87ea7846d4e26faffd251d8c
Jena Reis <jenadreis@gmail.com>
2021-12-21 23:22:38 -0500

    final

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,24 +23,49 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(stop())
+            }, time)
+        })
+        .then(() => console.log('Done walking east.'))
     }
 
-    function walkNorth() {
+
+    function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(stop())
+            }, time)
+        })
+        .then(() => console.log('Done walking north.'))
     }
 
-    function walkWest() {
+    function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(stop())
+            }, time)
+        })
+        .then(() => console.log('Done walking west.'))
     }
 
-    function walkSouth() {
+    function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(stop())
+            }, time)
+        })
+        .then(() => console.log('Done walking south.'))
     }
 
     function stop() {
